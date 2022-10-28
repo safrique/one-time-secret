fdfc619634f9c3250d759e5f9376225cd218989c
Josef van Niekerk <safrique@users.noreply.github.com>
2022-10-28 02:14:31 +0100

    Added command to delete expired secrets

---
+++ app/Console/Commands/DeleteSecretCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Secret;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class DeleteSecretCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'secret:delete';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes expired commands';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    : int
+    {
+        if (($secrets = Secret::where('delete_at', '<', Carbon::now()))->exists()) {
+            foreach ($secrets->get() as $secret) {
+                $secret->delete();
+            }
+        }
+        return 0;
+    }
+}
